626284ac18a59c00328419d5823b3717582e36cf
macul99 <23519286+macul99@users.noreply.github.com>
2024-03-01 16:16:08 +0800

    Update dynamic_programming.py

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -11,3 +11,23 @@ def fibseq(n):
         
     return result
 
+def iter_matrix(i, j, v, m, N, K):
+    print(": ", i, j)
+    v_temp = v + j - i + 1
+    
+    if v_temp == N: # reach the goal
+        print(i, j)
+        return v_temp
+    
+    if m==K-1: # reach max K value
+        if v<N: # invalid skip, continue iter
+            print('invalid')
+            return None
+    
+    # loop next row with i+1, j+1
+    for k in range(j+1, N):
+        #print(i+1, k)
+        rst = iter_matrix(j+1, k, v_temp, m+1, N, K)
+        if rst is not None:
+            print(i,j)
+        #return rst
